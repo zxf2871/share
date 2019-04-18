d64ae3a83a7dcd2aecf345f2aabacfad84eb08eb
zhaoxinfu <zhaoxinfu@zhaoxinfudeMac-mini.local>
2019-04-18 22:15:51 +0800

    2

--- app/src/main/java/com/example/share/MainActivity.java
+++ app/src/main/java/com/example/share/MainActivity.java
@@ -2,12 +2,85 @@ package com.example.share;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.webview);
+        initSetting();
+        initChromClient();
+        initWebViewClient();
+        start();
+    }
+
+
+    private void initSetting(){
+        WebSettings settings =  mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+    }
+
+    private void initChromClient() {
+        mWebView.addJavascriptInterface(new JS(), "nameSpace");
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            //监听alert框
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+
+            //监听confirm框
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                return super.onJsConfirm(view, url, message, result);
+            }
+
+            //监听Prompt
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
+        });
+    }
+
+    private void initWebViewClient(){
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+    }
+
+
+    private void start() {
+        mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public class JS{
+
+        @JavascriptInterface
+        public void callAndroidMethod(String message){
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
